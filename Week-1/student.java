d32d2e588baf00967f5542217e03cb54e7da43a3
chandra2006-tech <chandraranamalla68@gmail.com>
2025-03-07 10:31:43 +0530

    Add files via upload

---
+++ Week-1/student.java
@@ -0,0 +1,9 @@
+class student
+{
+	public static void main(String[] args)
+{
+        System.out.println("Chandra");
+	System.out.println("Class :CSE-C");
+	System.out.println("Roll No:24247");
+}
+}
